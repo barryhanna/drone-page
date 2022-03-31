d1cc868dc99470103758731dbc5144a9bc9f3256
Barry Hanna <barryhanna@users.noreply.github.com>
2022-03-31 11:20:09 +0100

    Completed HTML and added menu

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // javascript
+const closeMobileMenuBtn = document.querySelector('.close-mobile-menu');
+const openMobileMenuBtn = document.querySelector('.open-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openMobileMenuBtn.addEventListener('click', (e) => {
+  mobileMenu.classList.add('open');
+});
+
+closeMobileMenuBtn.addEventListener('click', (e) => {
+  mobileMenu.classList.remove('open');
+});
